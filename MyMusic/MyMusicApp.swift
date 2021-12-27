cc4a68914c460b3ea58c69ecf16b39a91d43d593
shoishizaki <shoishizaki1997@gmail.com>
2021-12-28 03:03:11 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by 石崎將 on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
